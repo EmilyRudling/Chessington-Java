70d667d0dd89d4a14e2f11535008dc1f0f01b040
Emily Rudling <mmrudling@yahoo.com>
2020-09-24 14:05:23 +0100

    Finished first two tests for pawn

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -15,6 +15,32 @@ public class Pawn extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        List<Move> allowedMoves = new ArrayList<>();
+        if (this.getColour() == PlayerColour.WHITE){
+            allowedMoves.add(new Move(from, (new Coordinates(from.getRow() -1, from.getCol()))));
+        }
+        if (this.getColour() == PlayerColour.BLACK){
+            allowedMoves.add(new Move(from, (new Coordinates(from.getRow() +1, from.getCol()))));
+        }
+        return allowedMoves;
+    }
+
+
+
+
+
+    public void CanMovePawn(Coordinates to, Coordinates from){
+        //checks if it is an allowed move
+        //if it is one square, MovePawnOne, if two MovePawnTwo
+    }
+
+    public void MovePawnOne(Coordinates to, Coordinates from){
+        //moves the pawn to the correct place
+        //if the space was occupied by an enemy piece, remove it from the board
+    }
+
+    public void MovePawnTwo(Coordinates to, Coordinates from){
+        //moves the pawn to the correct place
+        //if the space was occupied by an enemy piece, remove it from the board
     }
 }
--- src/test/java/training/chessington/model/GameTest.java
+++ src/test/java/training/chessington/model/GameTest.java
@@ -1,7 +1,25 @@
 package training.chessington.model;
 
+import org.junit.Test;
+
+import static training.chessington.model.pieces.Piece.PieceType.PAWN;
 import static training.chessington.model.pieces.PieceAssert.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class GameTest {
+    @Test
+    public void canMovePawnOneSpace() {
+        // Arrange
+        Board board = Board.forNewGame();
+
+        Coordinates from = new Coordinates(6, 0);
+        Coordinates to = new Coordinates(5, 0);
+
+        // Act
+        board.move(from, to);
+
+        // Assert
+        assertThat(board.get(from)).isNull();
+        assertThat(board.get(to)).isColour(PlayerColour.WHITE).isPiece(PAWN);
+    }
 }
